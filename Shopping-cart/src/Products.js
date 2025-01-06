6be4f0830a068e43debb1da4025f63967db4e9a1
Mosesvictor1 <federaluk0@gmail.com>
2025-01-06 22:00:03 +0100

    2025

---
+++ Shopping-cart/src/Products.js
@@ -0,0 +1,108 @@
+/*============ Build our products data ==============*/
+
+export const productsData = [
+  {
+    id: 1,
+    title: "moses 2 yards",
+    price: 2000,
+    imageUrl: "images/ch.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Carton Astronaut T-Shirt",
+    price: 300,
+    imageUrl: "images/ch2.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Carton Astronaut T-Shirt",
+    price: 300,
+    imageUrl: "images/ch3.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Carton Astronaut T-Shirt",
+    price: 95,
+    imageUrl: "images/ch4.jpg",
+  },
+
+  {
+    id: 5,
+    title: "Carton Astronaut T-Shirt",
+    price: 3500,
+    imageUrl: "images/ch5.jpg",
+  },
+
+  {
+    id: 6,
+    title: "Carton Astronaut T-Shirt",
+    price: 12,
+    imageUrl: "images/ch6.jpg",
+  },
+
+  {
+    id: 7,
+    title: "Carton Astronaut T-Shirt",
+    price: 500,
+    imageUrl: "images/ch7.jpg",
+  },
+
+  {
+    id: 8,
+    title: "Carton Astronaut T-Shirt",
+    price: 500,
+    imageUrl: "images/ch7.jpg",
+  },
+
+  {
+    id: 9,
+    title: "Carton Astronaut T-Shirt",
+    price: 129,
+    imageUrl: "images/ch1.jpg",
+  },
+
+  {
+    id: 10,
+    title: "Carton Astronaut T-Shirt",
+    price: 650,
+    imageUrl: "images/ch2.jpg",
+  },
+
+  {
+    id: 11,
+    title: "Carton Astronaut T-Shirt",
+    price: 700,
+    imageUrl: "images/ch4.jpg",
+  },
+
+  {
+    id: 12,
+    title: "Carton Astronaut T-Shirt",
+    price: 119,
+    imageUrl: "images/ch6.jpg",
+  },
+
+  {
+    id: 13,
+    title: "Carton Astronaut T-Shirt",
+    price: 400,
+    imageUrl: "images/ch1.jpg",
+  },
+
+  {
+    id: 14,
+    title: "Carton Astronaut T-Shirt",
+    price: 500,
+    imageUrl: "images/ch3.jpg",
+  },
+
+  {
+    id: 15,
+    title: "Carton Astronaut T-Shirt",
+    price: 350,
+    imageUrl: "images/ch5.jpg",
+  },
+];
